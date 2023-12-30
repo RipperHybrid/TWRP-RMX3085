95cf290e7427d3ca94fd944be1ad807f26e4dd45
AshBorn <anishsingh0119@gmail.com>
2023-12-30 19:06:18 +0530

    Add files via upload

--- recovery/root/system/bin/_set_codename.sh
+++ recovery/root/system/bin/_set_codename.sh
@@ -1,8 +1,17 @@
 #!/sbin/sh
 
 set_device_codename() {
-resetprop "ro.product.device" "$1"
-        for i in odm product system system_ext vendor; do
-         resetprop "ro.product.${i}.device" "$1"
-        done
+    new_codename="$1"
+    
+    echo "- Changing device codename to: $new_codename"
+    echo " "
+
+    resetprop "ro.product.device" "$new_codename"
+    
+    for i in odm product system system_ext vendor; do
+        resetprop "ro.product.${i}.device" "$new_codename"
+    done
+
+    echo "- Device codename set to: $new_codename"
+    echo " "
 }
